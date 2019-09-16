54035380ca4841e0cc57318f9029e222363f5ad5
unicei <49039267+unicei@users.noreply.github.com>
2019-09-16 08:37:03 -0300

    Update structor.cpp

--- structor.cpp
+++ structor.cpp
@@ -1,4 +1,4 @@
-#include <sculptorh.h>
+#include <sculptor.h>
 #include <iostream>
 using namespace std;
 
@@ -7,32 +7,50 @@ using namespace std;
 Sculptor::Sculptor(int _nx, int _ny, int _nz)
 {
 
-    Voxel ***arr = new Voxel**[_nx];
+    Voxel ***v = new Voxel**[_nx];
     for (int i = 0; i < _nz; ++i) {
-      arr[i] = new Voxel*[_ny];
+      v[i] = new Voxel*[_ny];
       for (int j = 0; j < _ny; ++j)
-        arr[i][j] = new Voxel[_nz];
+        v[i][j] = new Voxel[_nz];
     }
 }
 
-Sculptor::~Sculptor()
-{
-    
+Sculptor::~Sculptor(){
+
+    for(int i=0;i<nz;i++){
+        for(int j=0;j<ny;j++){
+            delete v[i][j];
+        }
+    }
+    for(int i=0;i<nz;i++){
+        delete v[i];
+    }
+    delete v;
 }
 
 void Sculptor::setColor(float r, float g, float b, float alpha)
 {
     this-> r = r;
     this-> g = g;
+    this-> b = b;
     a = alpha;
 }
 
-void Sculptor::putVoxel(int x, int y, int z)
-{
-
+void Sculptor::putVoxel(int x, int y, int z){
+   if(x<=nx && y<=ny && z<=nz){
+    v[x][y][z].r=r;
+    v[x][y][z].g=g;
+    v[x][y][z].b=b;
+    v[x][y][z].a=a;
+    v[x][y][z].isOn=true;
+  }
+   else{
+       v[x][y][z].isOn=false;
+   }
 }
 
-void Sculptor::cutVoxel(int x, int y, int z)
-{
+void Sculptor::cutVoxel(int x, int y, int z){
 
+
+     v[x][y][z].isOn=false;
 }
